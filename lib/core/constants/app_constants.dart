884621ea83204979c218cb141d404d79f891089f
akaanuzman <uzmanakan@gmail.com>
2022-12-07 14:56:30 +0300

    my core structre was added.

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/single_child_widget.dart';
+
+import '../utils/navigation_service.dart';
+
+class AppConstants {
+  static AppConstants? _instance;
+  static AppConstants get instance {
+    if (_instance != null) {
+      return _instance!;
+    } else {
+      _instance = AppConstants.init();
+      return _instance!;
+    }
+  }
+
+  AppConstants.init();
+
+  String get appTitle => "Todo App";
+  bool get debugShowCheckedModeBanner => false;
+  get localizationsDelegates => AppLocalizations.localizationsDelegates;
+  get supportedLocales => AppLocalizations.supportedLocales;
+  get navigatorKey => NavigationService.navigatorKey;
+
+  final List<SingleChildWidget> _providers = [
+
+  ];
+
+  List<SingleChildWidget> get providers => _providers;
+}
---
+++ lib/products/views/auth/login_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.test),
+      ),
+    );
+  }
+}
